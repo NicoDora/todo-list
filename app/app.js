f806548ac07ba8bd1bc9631bf70793a345a67af0
NicoDora <pjh76574734@gmail.com>
2023-06-29 10:38:05 +0900

    프론트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -5,11 +5,12 @@ const express = require("express");
 const app = express();
 
 // 라우팅
-const main = require("./routes/home");
+const main = require("./src/routes/home");
 
 // 앱 세팅
-app.set("views", "./views"); // views 폴더를 사용
+app.set("views", "./src/views"); // views 폴더를 사용
 app.set("view engine", "ejs"); // view engine을 ejs로 사용
+app.use(express.static(`${__dirname}/src/public`)); // 정적 파일을 사용하기 위한 폴더 지정
 
 app.use("/", main); // 미들웨어 등록
 
